9e8d129e40e0b1ebcc0a83cd32459167fcfcbe79
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-03 17:11:19 -0300

    Add project files.

---
+++ BookStore/Models/Livro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {
+        public Livro()
+        {
+            this.Autores = new List<Autor>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo Inválido")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo Inválido")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        // public virtual int CategoriaId { get; set; }
+        // carrega CategoriaId sob demanda
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
