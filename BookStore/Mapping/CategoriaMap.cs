13748e6f5f18882b3e36f59ba59af772de1d88ae
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-03 18:02:41 -0300

    Creating mapping of Livro, Autor and Categoria.

---
+++ BookStore/Mapping/CategoriaMap.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Domain;
+using System.Data.Entity.ModelConfiguration;
+
+namespace BookStore.Mapping
+{
+    public class CategoriaMap 
+        : EntityTypeConfiguration<Categoria>
+    {
+        public CategoriaMap()
+        {
+            ToTable("Categoria");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Nome)
+                .HasMaxLength(30).IsRequired();
+
+            HasMany(x => x.Livos)
+                .WithRequired(x => x.Categoria);
+        }
+    }
+}
