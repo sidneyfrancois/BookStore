431512a320067afc0ed86a5cb7c0e4658a376b96
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-04 10:44:49 -0300

    Add Autor controller and its views. Using custom routes.

---
+++ BookStore/Controllers/AutorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    [RoutePrefix("autor")]
+    [Route("{Action=listar}")]
+    public class AutorController : Controller
+    {
+        [Route("listar")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("editar/{id:int}")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [Route("excluir/{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
