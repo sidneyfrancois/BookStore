0c5bcdd42cf9d8ec3b4ca84c622478532e5054d0
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-04 18:16:46 -0300

    Add ActionFilter as Global Filter, executing in all actions.

--- BookStore/Controllers/AutorController.cs
+++ BookStore/Controllers/AutorController.cs
@@ -4,7 +4,7 @@ using System.Web.Mvc;
 namespace BookStore.Controllers
 {
     [RoutePrefix("autores")]
-    [LogActionFilter()]
+    //[LogActionFilter()]
     public class AutorController : Controller
     {
         [Route("listar")]
