62d31de8cc15256fe1da4983c68b899f1171601d
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-04 09:26:06 -0300

    Add TesteController and Dados View.

---
+++ BookStore/Controllers/TesteController.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Domain;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class TesteController : Controller
+    {
+        public ViewResult Dados(int Id)
+        {
+            var autor = new Autor
+            {
+                Id = 1,
+                Nome = "Sidney"
+            };
+
+            ViewBag.Categoria = "Produtos de Limpeza";
+            ViewData["Categoria"] = "Produtos de Informática";
+            TempData["Categoria"] = "Produtos de Escritório";
+            Session["Categoria"] = "Móveis";
+
+            return View(autor);
+        }
+
+        public string Index()
+        {
+            return "teste dois";
+        }
+
+        [HttpPost]
+        [ActionName("Autor")]
+        public JsonResult ActionDois(Autor autor)
+        {
+            return Json(autor);
+        }
+    }
+}
