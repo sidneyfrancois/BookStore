3a143e0270dabbc968e78bcfd4f6ab8a5a5974ed
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-05 13:50:50 -0300

    Add Repository Pattern. Add Autor repository interface.

---
+++ BookStore/Repositories/Contracts/IAutorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Repositories.Contracts
+{
+    public interface IAutorRepository : IDisposable
+    {
+        List<Autor> Get();
+        Autor Get(int id);
+        Autor GetByName(string name);
+        bool Create(Autor autor);
+        bool Update(Autor autor);
+        void Delete(int id);
+    }
+}
