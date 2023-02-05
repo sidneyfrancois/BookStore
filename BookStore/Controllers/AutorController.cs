0f1ba69512796881b390f3918e2b584c0343a8b7
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-05 14:49:40 -0300

    Add call to bd into controllers.

--- BookStore/Controllers/AutorController.cs
+++ BookStore/Controllers/AutorController.cs
@@ -1,16 +1,25 @@
-﻿using BookStore.Filters;
+﻿using BookStore.Domain;
+using BookStore.Repositories;
+using BookStore.Repositories.Contracts;
 using System.Web.Mvc;
 
 namespace BookStore.Controllers
 {
     [RoutePrefix("autores")]
-    //[LogActionFilter()]
     public class AutorController : Controller
     {
+        private IAutorRepository repository;
+
+        public AutorController()
+        {
+            repository = new AutorRepository();
+        }
+
         [Route("listar")]
         public ActionResult Index()
         {
-            return View();
+            var autores = repository.Get();
+            return View(autores);
         }
 
         [Route("criar")]
@@ -19,16 +28,47 @@ namespace BookStore.Controllers
             return View();
         }
 
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(Autor autor)
+        {
+            if (repository.Create(autor))
+                return RedirectToAction("Index");
+
+            return View(autor);
+        }
+
         [Route("editar/{id:int}")]
         public ActionResult Edit(int id)
         {
-            return View();
+            var autor = repository.Get(id);
+            return View(autor);
+        }
+
+        [Route("editar/{id:int}")]
+        [HttpPost]
+        public ActionResult Edit(Autor autor)
+        {
+            if (repository.Update(autor))
+                return RedirectToAction("Index");
+
+            return View(autor);
         }
 
         [Route("excluir/{id:int}")]
         public ActionResult Delete(int id)
         {
-            return View();
+            var autor = repository.Get(id);
+            return View(autor);
+        }
+        
+        [Route("excluir/{id:int}")]
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
