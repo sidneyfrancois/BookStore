f7ef8fa7ff98c97d47a0c850a0c076d524e4a434
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-05 17:15:34 -0300

    Add Livro controller. Add Liro ViewModel.

---
+++ BookStore/Controllers/LivroController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    [RoutePrefix("livros")]
+    public class LivroController : Controller
+    {
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/ViewModels/CriarLivroViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.ViewModels
+{
+    public class CriarLivroViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public int CategoriaId { get; set; }
+    }
+}
