dfa8a7942bcbb0b7514a00721b2d224139d5243e
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-05 14:00:49 -0300

    Add IAutorRepository implementation

---
+++ BookStore/Repositories/AutorRepository.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Domain;
+using BookStore.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Repositories
+{
+    public class AutorRepository : IAutorRepository
+    {
+        public bool Create(Autor autor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Autor> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Autor Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Autor GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Autor autor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
